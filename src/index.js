f122d21ff1bd9c01a8839771160cd7fe654df366
zeze.li <lizeze.boy@gmail.com>
2019-11-29 17:22:20 +0800

    add token

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 var Cesium = require('cesium/Cesium');
 require('cesium/Widgets/widgets.css');
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MzJlOTlkMS00NDk1LTQ5OWQtOWNhMy01NjgyMDdlNmJjOWMiLCJpZCI6MTg0MzEsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzM3OTgwMDR9.kJuWRLeWtdVS_ESyxMLDXiMa6uaYQfPQ9-zxxbJJztE';
 var viewer = new Cesium.Viewer('cesiumContainer');
